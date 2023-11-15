7862c81a98c31f8dc77b3b9c9f3c10d8b645ecc3
estarlinsoto <141869663+estarlinsoto@users.noreply.github.com>
2023-11-15 10:31:27 +0000

    se agregaron el 88% de las tablas

--- src/models.py
+++ src/models.py
@@ -1,32 +1,69 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False )
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False )
+
+    #relationships
+    user = relationship('User', back_populates="Follower", single_parent=True, 
+    uselist=False, cascade="all, delete-orphan")
+   
+
+    
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(255))
+    firts_name = Column(String(20))
+    email = Column(String(200))
+    last_name = Column(String(100))
+    adderess = Column(String(255))
+
+     #relationships
+    follower_id = relationship('Follower', back_populates="user", 
+    single_parent=True, cascade="all,delete-orphan")
+    commets = relationship("Comment", back_populates="User",cascade="all,delete-orphan")
+    
+
+class  Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    follower_id = Column(Integer)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False) 
+    latitude = Column(String(50))
+    
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(50), nullable=False)
+    url = Column(String(50), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
 
     def to_dict(self):
         return {}
+
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
